34d979d5c18b05b05364b656b1db5a4e722720f5
faustinopsy <rodrigohipnose@gmail.com>
2024-08-01 11:59:08 -0300

    rate limite

--- proxy/index.php
+++ proxy/index.php
@@ -1,5 +1,72 @@
 <?php
 
+$rateLimit = 10; // Número máximo de requisições permitidas
+$rateLimitTime = 10; // Janela de tempo em segundos
+$penaltyTime = 300; // Tempo de penalidade em segundos (por exemplo, 5 minutos)
+
+$clientIp = $_SERVER['REMOTE_ADDR'];
+
+// Função para sanitizar o endereço IP para uso como nome de arquivo
+function sanitizeIp($ip) {
+    // Substitui dois-pontos e outros caracteres por sublinhados
+    return str_replace([':', '.'], '_', $ip);
+}
+
+$rateLimitFile = __DIR__ . "/rate_limit/" . sanitizeIp($clientIp) . ".txt";
+
+// Função para obter o número atual de requisições do IP
+function getRequestCount($rateLimitFile, $rateLimitTime, $penaltyTime) {
+    if (!file_exists($rateLimitFile)) {
+        return [0, 0];
+    }
+    $data = file_get_contents($rateLimitFile);
+    $requestInfo = explode(':', $data);
+    $requestTime = (int)$requestInfo[0];
+    $requestCount = (int)$requestInfo[1];
+    $blockedUntil = isset($requestInfo[2]) ? (int)$requestInfo[2] : 0;
+
+    // Verifica se o IP está bloqueado
+    if (time() < $blockedUntil) {
+        return [-1, $blockedUntil];
+    }
+
+    // Redefine o contador se a janela de tempo tiver passado
+    if (time() - $requestTime > $rateLimitTime) {
+        return [0, 0];
+    }
+    return [$requestCount, 0];
+}
+
+// Função para incrementar o contador de requisições
+function incrementRequestCount($rateLimitFile, $rateLimitTime, $penaltyTime) {
+    list($requestCount, $blockedUntil) = getRequestCount($rateLimitFile, $rateLimitTime, $penaltyTime);
+    if ($requestCount === -1) {
+        // IP está bloqueado
+        return;
+    }
+
+    $requestTime = time();
+    $newBlockedUntil = ($requestCount + 1 >= $GLOBALS['rateLimit']) ? $requestTime + $penaltyTime : 0;
+    file_put_contents($rateLimitFile, $requestTime . ':' . ($requestCount + 1) . ':' . $newBlockedUntil);
+}
+
+// Verificar a limitação de taxa
+list($requestCount, $blockedUntil) = getRequestCount($rateLimitFile, $rateLimitTime, $penaltyTime);
+if ($requestCount === -1) {
+    http_response_code(429); // Muitas requisições
+    echo json_encode(["message" => "temporariamente bloqueado. Tente novamente em " . ($blockedUntil - time()) . " segundos."]);
+    exit();
+}
+
+if ($requestCount >= $rateLimit) {
+    http_response_code(429); // Muitas requisições
+    echo json_encode(["message" => "Limite de requisições excedido. Tente novamente mais tarde."]);
+    exit();
+}
+
+// Incrementar o contador de requisições
+incrementRequestCount($rateLimitFile, $rateLimitTime, $penaltyTime);
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
